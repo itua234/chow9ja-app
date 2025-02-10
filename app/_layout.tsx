da19f8a899ff62e9f336d71151bd9b156e0f06ab
osemeilu itua <ituaosemeilu234@gmail.com>
2025-02-10 12:48:12 +0100

    resolved the user authorization issue, works perfectly now

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,19 @@
-import React, { useEffect, useLayoutEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { 
   DarkTheme, 
   DefaultTheme, 
   ThemeProvider 
 } from '@react-navigation/native';
-import { router, Stack } from 'expo-router';
+import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
 import { useFonts } from 'expo-font';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {SvgXml} from "react-native-svg";
-import {groundforce_logo} from '@/util/svg';
 
 import { store, RootState, AppDispatch } from '@/reducers/store';
 import { Provider, useDispatch, useSelector } from 'react-redux';
-import { setUser, setLoading, setAppIsReady, setisAuthenticated } from '@/reducers/auth/authSlice';
-import {get_user} from "@/api"
-import { User } from "@/models/User";
+import { setAppIsReady } from '@/reducers/auth/authSlice';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 import 'react-native-reanimated';
@@ -39,15 +34,6 @@ export default function Layout() {
 }
 
 function RootLayout() {
-  const dispatch = useDispatch<AppDispatch>();
-  const {
-    user,
-    isAuthenticated,
-    isLoading,
-    appIsReady
-  } = useSelector((state: RootState) => state.auth);
-  const [isFirstTime, setIsFirstTime] = useState<string | null>(null);
-
   const [fontsLoaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
     "Campton-Black": require('../assets/fonts/campton/CamptonBlack.otf'),
@@ -58,70 +44,14 @@ function RootLayout() {
     "Campton-ExtraBold": require('../assets/fonts/campton/CamptonExtraBold.otf')
   });
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     const initializeApp = async () => {
       if (fontsLoaded) {
-        await checkAuthStatus(); // Wait for auth status check
         await SplashScreen.hideAsync(); // Hide splash screen after everything is ready
       }
     };
     initializeApp();
   }, [fontsLoaded]);
-
-  // useEffect(() => {
-  //   (async () => {
-  //     const firstTimeValue = await AsyncStorage.getItem('isFirstTime');
-  //     const newValue = firstTimeValue === null ? "true" : "false";
-  //     setIsFirstTime(newValue);
-  //   })();
-  // }, []);
-
-  const checkAuthStatus = async () => {
-    //await AsyncStorage.clear();
-    try {
-      const token = await AsyncStorage.getItem('user_token');
-      if(token){
-        const response = await get_user();
-        const user: User = response.data.results;
-        dispatch(setUser(user));
-        dispatch(setAppIsReady(true));
-        dispatch(setisAuthenticated(true));
-        dispatch(setLoading(false));
-      }
-    }catch (error) {
-      console.log('Error checking auth status:', error);
-    }finally{
-      dispatch(setAppIsReady(true));
-      dispatch(setisAuthenticated(false));
-      dispatch(setLoading(false));
-    }
-  };
-
-  // useEffect(() => {
-  //   if (appIsReady && fontsLoaded) {
-  //     if (isAuthenticated) {
-  //       router.replace("/dashboard"); // Navigate only after app is ready
-  //     } else if (isFirstTime === 'false') {
-  //       router.replace("/sign-in");
-  //     }
-  //   }
-  // }, [appIsReady, isAuthenticated]); // Run when appIsReady or isAuthenticated changes
-
-  if (!fontsLoaded || !appIsReady) {
-    return (
-      <View className="
-      flex-1 
-      items-center 
-      justify-center
-      bg-[#4884DF]">
-        <SvgXml
-          xml={groundforce_logo}
-          width="200"
-          height="30"
-        />
-      </View>
-    );
-  }
 
   return (
     <Stack screenOptions={{ headerShown: false }}>
--- app/index.tsx
+++ app/index.tsx
@@ -16,15 +16,13 @@ import { SvgXml } from "react-native-svg";
 import { groundforce_logo } from '@/util/svg';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/reducers/store';
-import { useFonts } from "expo-font";
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { AppDispatch } from '@/reducers/store';
-import { Provider, useDispatch} from 'react-redux';
+import { useDispatch} from 'react-redux';
 import { setUser, setLoading, setAppIsReady, setisAuthenticated } from '@/reducers/auth/authSlice';
 import {get_user} from "@/api"
 import { User } from "@/models/User";
-import * as SplashScreen from 'expo-splash-screen';
 
 export default function index() {
     const dispatch = useDispatch<AppDispatch>();
@@ -34,54 +32,106 @@ export default function index() {
         isLoading,
         appIsReady
     } = useSelector((state: RootState) => state.auth);
-      
-    return (
-        <>
-        <StatusBar style="light" />
-        <View className="flex-1 flex-column bg-primary">
-            <View className="flex-1">
-                
+    const [isFirstTime, setIsFirstTime] = useState<string | null>(null);
+    useEffect(() => {
+        const initializeApp = async () => {
+            const firstTimeValue = await AsyncStorage.getItem('isFirstTime');
+            const newValue = firstTimeValue === null ? "true" : "false";
+            setIsFirstTime(newValue);
+            await checkAuthStatus(); // Wait for auth status check
+        };
+        initializeApp();
+    }, []);
+    const checkAuthStatus = async () => {
+        try {
+          const token = await AsyncStorage.getItem('user_token');
+          if(token){
+            const response = await get_user();
+            const user: User = response.data.results;
+            dispatch(setUser(user));
+            dispatch(setAppIsReady(true));
+            dispatch(setisAuthenticated(true));
+            dispatch(setLoading(false));
+          }
+        }catch (error) {
+          console.log('Error checking auth status:', error);
+        }finally{
+          dispatch(setAppIsReady(true));
+          dispatch(setLoading(false));
+        }
+    };
+    useEffect(() => {
+        // Redirect based on `isFirstTime` and `isAuthenticated`
+        if (appIsReady && isFirstTime === "false") {
+            if (isAuthenticated) {
+                router.replace("/dashboard");
+            } else {
+                router.replace("/sign-in");
+            }
+        }
+    }, [appIsReady, isFirstTime, isAuthenticated]);
+
+    if (!appIsReady || isFirstTime === null) {
+        return (
+            <View className="
+            flex-1 
+            items-center 
+            justify-center
+            bg-[#4884DF]">
+                <SvgXml
+                    xml={groundforce_logo}
+                    width="200"
+                    height="30"
+                />
             </View>
-            <View className="flex-1 bg-white px-[17.5] text-center pt-[40px]">
-                <View className="">
-                    <Text className="font-primary text-[27px] text-center">Stay Ahead: Manage Subscriptions with Ease!</Text>
-                    <Text className="font-primary text-[16px] text-center mt-[15px]">Get timely alerts for upcoming payments and price changes.</Text>
+        );
+    }
+    
+    if (isFirstTime === "true") {
+        return (
+            <>
+            <StatusBar style="light" />
+            <View className="flex-1 flex-column bg-primary">
+                <View className="flex-1">
+                    
                 </View>
-                <View className="mt-[20px]">
-                    <PrimaryButton 
-                        title="Sign Up Now"
-                        isLoading={false} 
-                        action={async () => {
-                            await Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-                            router.push("/sign-up");
-                        }}
-                        disabled={false}
-                    />
-                    <Pressable className=" 
-                        px-[15px] 
-                        rounded-[15px] 
-                        mt-6 
-                        flex 
-                        items-center 
-                        justify-center" 
-                        onPress={async () => {
-                            await Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-                            router.push("/sign-in");
-                        }}>
-                        <Text className="
-                            text-center 
-                            font-primary
-                            text-[18px]
-                        " >Log In</Text>
-                    </Pressable>
+                <View className="flex-1 bg-white px-[17.5] text-center pt-[40px]">
+                    <View className="">
+                        <Text className="font-primary text-[27px] text-center">Stay Ahead: Manage Subscriptions with Ease!</Text>
+                        <Text className="font-primary text-[16px] text-center mt-[15px]">Get timely alerts for upcoming payments and price changes.</Text>
+                    </View>
+                    <View className="mt-[20px]">
+                        <PrimaryButton 
+                            title="Sign Up Now"
+                            isLoading={false} 
+                            action={async () => {
+                                await Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+                                router.push("/sign-up");
+                            }}
+                            disabled={false}
+                        />
+                        <Pressable className=" 
+                            px-[15px] 
+                            rounded-[15px] 
+                            mt-6 
+                            flex 
+                            items-center 
+                            justify-center" 
+                            onPress={async () => {
+                                await Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+                                router.push("/sign-in");
+                            }}>
+                            <Text className="
+                                text-center 
+                                font-primary
+                                text-[18px]
+                            " >Log In</Text>
+                        </Pressable>
+                    </View>
                 </View>
             </View>
-        </View>
-        </>
-    );
+            </>
+        );
+    }
 
 }
-
-const styles = StyleSheet.create({
-    
-});
